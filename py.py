f8fcc4338d2986825ef702d18c9374ff016d87a6
Екатерина <150816212+Azazel1669@users.noreply.github.com>
2024-02-27 20:24:52 +0300

    Шарики

--- py.py
+++ py.py
@@ -1,41 +1,52 @@
 import pygame
-import colorsys
-
-
-def draw(screen):
-    screen.fill("black")
-    color = (255, 255, 255)
-    color0 = pygame.Color(color)
-    color1 = pygame.Color(color)
-    color2 = pygame.Color(color)
-    color0.hsva = (Hue, 100, 100)
-    color1.hsva = (Hue, 100, 75)
-    color2.hsva = (Hue, 100, 50)
-    pygame.draw.polygon(screen, color0,
-                        [((300 - W) // 2, (300 - W) // 2), ((300 - W) // 2 + W // 2, (300 - W) // 2 - W // 2),
-                         ((300 - W) // 2 + W // 2 * 3, (300 - W) // 2 - W // 2), ((300 - W) // 2 + W, (300 - W) // 2)])
-    screen.fill(pygame.Color(color1), pygame.Rect((300 - W) // 2, (300 - W) // 2, W, W))
-    pygame.draw.polygon(screen, color2,
-                        [((300 - W) // 2 + W // 2 * 3, (300 - W) // 2 + W // 2),
-                         ((300 - W) // 2 + W // 2 * 3, (300 - W) // 2 + W // 2), (W + (300 - W) // 2, (300 - W) // 2 + W),
-                         ((300 - W) // 2 + W, (300 - W) // 2), ((300 - W) // 2 + W // 2 * 3, (300 - W) // 2 - W // 2)])
-
 
 if __name__ == '__main__':
     pygame.init()
-    pygame.display.set_caption("Куб")
-    try:
-        W, Hue = map(int, input().split())
-        if W % 4 != 0 or Hue > 360 or Hue < 0 or W > 100:
-            raise ValueError
-    except ValueError:
-        print("Неправильный формат ввода")
-        pygame.quit()
-    else:
-        size = width, height = 300, 300
-        screen = pygame.display.set_mode(size)
-        draw(screen)
+    pygame.display.set_caption('Движущийся круг 2')
+    size = width, height = 800, 400
+    screen = pygame.display.set_mode(size)
+
+    running = True
+    k = 0
+    v = 20  # пикселей в секунду
+    fps = 60
+    a = []
+    clock = pygame.time.Clock()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                k += 1
+                x_pos = list(event.pos)[0]
+                y_pos = list(event.pos)[1]
+                a.append([x_pos, y_pos, 0, 0])
+        if k != 0:
+            screen.fill((0, 0, 0))
+            k1, k0 = -1, -1
+            for i in a:
+                k0 += 1
+                for j in i:
+                    pygame.draw.circle(screen, (255, 0, 0), (a[k0][0], a[k0][1]), 20)
+                    if a[k0][0] == 0:
+                        a[k0][2] = 1
+                    if a[k0][1] == 0:
+                        a[k0][3] = 1
+                    if a[k0][0] == 800:
+                        a[k0][2] = 0
+                    if a[k0][1] == 400:
+                        a[k0][3] = 0
+                    if a[k0][2] == 0:
+                        a[k0][0] -= 1
+                    if a[k0][3] == 0:
+                        a[k0][1] -= 1
+                    if a[k0][2] == 1:
+                        a[k0][0] += 1
+                    if a[k0][3] == 1:
+                        a[k0][1] += 1
+
+
+
+        clock.tick(fps)
         pygame.display.flip()
-        while pygame.event.wait().type != pygame.QUIT:
-            pass
-        pygame.quit()
+    pygame.quit()
