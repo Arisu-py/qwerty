165917a9c7fa67c3517637950d02f7ced17372ef
Екатерина <150816212+Azazel1669@users.noreply.github.com>
2024-02-27 20:22:34 +0300

    Куб

--- py.py
+++ py.py
@@ -1,24 +1,41 @@
 import pygame
+import colorsys
 
 
 def draw(screen):
-    screen.fill("white")
-    for i in range(12):
-        for j in range(11):
-            if i % 2 == 0:
-                screen.fill(pygame.Color('red'), pygame.Rect(j * 32, i * 17, 30, 15))
-            else:
-                screen.fill(pygame.Color('red'), pygame.Rect(j * 32 - 15, i * 17, 30, 15))
-                screen.fill(pygame.Color('white'), pygame.Rect(15, 17 * i, 2, 15))
+    screen.fill("black")
+    color = (255, 255, 255)
+    color0 = pygame.Color(color)
+    color1 = pygame.Color(color)
+    color2 = pygame.Color(color)
+    color0.hsva = (Hue, 100, 100)
+    color1.hsva = (Hue, 100, 75)
+    color2.hsva = (Hue, 100, 50)
+    pygame.draw.polygon(screen, color0,
+                        [((300 - W) // 2, (300 - W) // 2), ((300 - W) // 2 + W // 2, (300 - W) // 2 - W // 2),
+                         ((300 - W) // 2 + W // 2 * 3, (300 - W) // 2 - W // 2), ((300 - W) // 2 + W, (300 - W) // 2)])
+    screen.fill(pygame.Color(color1), pygame.Rect((300 - W) // 2, (300 - W) // 2, W, W))
+    pygame.draw.polygon(screen, color2,
+                        [((300 - W) // 2 + W // 2 * 3, (300 - W) // 2 + W // 2),
+                         ((300 - W) // 2 + W // 2 * 3, (300 - W) // 2 + W // 2), (W + (300 - W) // 2, (300 - W) // 2 + W),
+                         ((300 - W) // 2 + W, (300 - W) // 2), ((300 - W) // 2 + W // 2 * 3, (300 - W) // 2 - W // 2)])
 
 
 if __name__ == '__main__':
     pygame.init()
-    pygame.display.set_caption("Кирпичи")
-    size = width, height = 300, 200
-    screen = pygame.display.set_mode(size)
-    draw(screen)
-    pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
-    pygame.quit()
+    pygame.display.set_caption("Куб")
+    try:
+        W, Hue = map(int, input().split())
+        if W % 4 != 0 or Hue > 360 or Hue < 0 or W > 100:
+            raise ValueError
+    except ValueError:
+        print("Неправильный формат ввода")
+        pygame.quit()
+    else:
+        size = width, height = 300, 300
+        screen = pygame.display.set_mode(size)
+        draw(screen)
+        pygame.display.flip()
+        while pygame.event.wait().type != pygame.QUIT:
+            pass
+        pygame.quit()
