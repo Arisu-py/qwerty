c4636882a36f8c9d0eae696975455f3fdb977843
Arisu <qwerty@qw.ry>
2024-02-27 21:47:48 +0400

    p52

--- p2.py
+++ p2.py
@@ -1,23 +1,22 @@
-import unittest
+import pytest
 from yandex_testing_lesson import reverse
 
+def test_empty():
+    assert reverse('') == ''
 
-class TestReverse(unittest.TestCase):
-    def test_empty(self):
-        self.assertEqual(reverse(''), '')
-    def test_one(self):
-        self.assertEqual(reverse('a'), 'a')
-    def test_palindrom(self):
-        self.assertEqual(reverse('aba'), 'aba')
-    def test_ordinary(self):
-        self.assertEqual(reverse('abcde'), 'edcba')
-    def test_wrong_not_iter(self):
-        with self.assertRaises(TypeError):
-            reverse(42)
-    def test_wrong_iter(self):
-        with self.assertRaises(TypeError):
-            reverse(['a', 'b', 'c'])
+def test_one():
+    assert reverse('a') == 'a'
 
+def test_palindrom():
+    assert reverse('aba') == 'aba'
 
-if __name__ == '__main__':
-     unittest.main()
+def test_ordinary():
+    assert reverse('abc') == 'cba'
+
+def test_wrong_not_iter():
+    with pytest.raises(TypeError):
+        reverse(42)
+
+def test_wrong_iter():
+    with pytest.raises(TypeError):
+        reverse(['a', 'b', 'c'])
