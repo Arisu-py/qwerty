4807f90e1304bbba524afc9fbfb69d37a62cc7fd
Arisu <qwerty@qw.ry>
2024-02-27 21:30:12 +0400

    p33

--- p2.py
+++ p2.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 
 
@@ -7,11 +9,12 @@ class Board:
         self.width = board_width
         self.height = board_height
         self.board = [[None] * self.width for _ in range(self.height)]
-
+        # значения по умолчанию
         self.left = 50
         self.top = 10
         self.cell_size = 30
 
+    # настройка внешнего вида
     def set_view(self, left, top, cell_size):
         self.left = left
         self.top = top
@@ -23,9 +26,13 @@ class Board:
                 r = (self.left + j * self.cell_size,
                      self.top + i * self.cell_size,
                      self.cell_size, self.cell_size)
+                # r_b = (r[0] + 1, r[1] + 1, r[2] - 2, r[3] - 2)
                 if self.board[i][j] is not None:
+                    # r_inner = (r_b[0] + 1, r_b[1] + 1,
+                    #            r_b[2] - 2, r_b[3] - 2)
                     pygame.draw.rect(screen, self.board[i][j], r)
                 pygame.draw.rect(screen, (255, 255, 255), r, 1)
+
 
     def get_cell(self, mouse_pos):
         row = (mouse_pos[1] - self.top) // self.cell_size
@@ -33,9 +40,21 @@ class Board:
         return (row, col) if (0 <= row < self.height and
                               0 <= col < self.width) else None
 
+    def on_click(self, cell):
+        color = (0, 0, 0)
+        for i in range(self.height):
+            if self.board[i][cell[1]] is None or self.board[i][cell[1]] == (0, 0, 0):
+                color = (255, 255, 255)
+            else:
+                color = (0, 0, 0)
+            self.board[i][cell[1]] = (color)
+        for j in range(self.width):
+            self.board[cell[0]][j] = (color)
+
     def process_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
-        print(cell)
+        if cell is not None:
+            self.on_click(cell)
 
 
 if __name__ == '__main__':
@@ -46,14 +65,19 @@ if __name__ == '__main__':
     running = True
     fps = 30
     clock = pygame.time.Clock()
-    board = Board(4, 3)
+    # MY_EVENT = pygame.USEREVENT + 1
+    # pygame.time.set_timer(MY_EVENT, 1000)
+    board = Board(7, 5)  # n = 5, m = 7
     board.set_view(20, 50, 100)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            # elif event.type == MY_EVENT:
+            #     # 123
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 board.process_click(event.pos)
+        # обновление экрана
         screen.fill((0, 0, 0))
         board.render(screen)
         pygame.display.flip()
