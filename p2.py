83c7704906ed536411fa4322527b647d41a4cc9f
Arisu <qwerty@qw.ry>
2024-02-27 21:48:17 +0400

    p52

--- p2.py
+++ p2.py
@@ -1,22 +1,11 @@
 import pytest
-from yandex_testing_lesson import reverse
+from yandex_testing_lesson import count_chars
+
 
 def test_empty():
-    assert reverse('') == ''
+    assert count_chars('aaabb') == {'a': 3, 'b': 2}
 
-def test_one():
-    assert reverse('a') == 'a'
-
-def test_palindrom():
-    assert reverse('aba') == 'aba'
-
-def test_ordinary():
-    assert reverse('abc') == 'cba'
 
 def test_wrong_not_iter():
     with pytest.raises(TypeError):
-        reverse(42)
-
-def test_wrong_iter():
-    with pytest.raises(TypeError):
-        reverse(['a', 'b', 'c'])
+        count_chars(42)
