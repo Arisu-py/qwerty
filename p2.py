c515c339a5881c97be54c324ee073d757b2d7188
Arisu-py <150816378+Arisu-py@users.noreply.github.com>
2024-04-06 21:11:44 +0400

    Update p2.py

--- p2.py
+++ p2.py
@@ -1,14 +1,16 @@
-# proverka oshibok
+# raschot pl
 import argparse
 
-
-def print_error(message):
-    print(f'ERROR: {message}!!')
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("message")
-    args = parser.parse_args()
-    print('Welcome to my program')
-    print_error(args.message)
+parser = argparse.ArgumentParser()
+parser.add_argument("--per-day", dest='per_day', type=int, default=0)
+parser.add_argument("--per-week", dest='per_week', type=int, default=0)
+parser.add_argument("--per-month", dest='per_month', type=int, default=0)
+parser.add_argument("--per-year", dest='per_year', type=int, default=0)
+parser.add_argument("--get-by", dest='get', type=str, default='day', choices=['day', 'month', 'year'])
+args = parser.parse_args()
+if args.get == 'day':
+    print(int(args.per_day + (args.per_week / 7) + (args.per_month / 30) + (args.per_year / 360)))
+elif args.get == 'month':
+    print(int(args.per_day * 30 + (args.per_week * 30 / 7) + args.per_month + args.per_year / 12))
+else:
+    print(int(args.per_day * 360 + (args.per_week * 360 / 7) + args.per_month * 12 + args.per_year))
