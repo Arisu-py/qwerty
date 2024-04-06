ea7dc75442b92d5654e87abcd4f7e1b9410b07e2
Arisu-py <150816378+Arisu-py@users.noreply.github.com>
2024-04-06 21:10:33 +0400

    Update p2.py

--- p2.py
+++ p2.py
@@ -1,28 +1,12 @@
-# mega cat
-import os
+# parsing slovarya
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("name")
+parser.add_argument("values", nargs='*')
 parser.add_argument('--sort', dest='sort', action='store_true')
-parser.add_argument('--num', dest='num', action='store_true')
-parser.add_argument('--count', dest='count', action='store_true')
 args = parser.parse_args()
-if os.access(args.name, os.F_OK):
-    with open(args.name) as in_file:
-        lines = [line.rstrip('\n') for line in in_file.readlines()]
-    if args.sort:
-        lines.sort()
-    f = True
-    if args.num:
-        for i in range(len(lines)):
-            print(i, lines[i])
-        f = False
-    if args.count:
-        if f:
-            print(*lines, sep='\n')
-        print(f'rows count: {len(lines)}')
-    if not (args.num or args.count):
-        print(*lines, sep='\n')
-else:
-    print('ERROR')
+sp = {a.split('=')[0]: a.split('=')[1] for a in args.values}
+if args.sort:
+    sp = dict(sorted(sp.items()))
+for a in sp:
+    print(f'Key: {a} Value: {sp[a]}')
