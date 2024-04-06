509e894742d0d9743e2b6c4b6284053f50c2d332
Arisu-py <150816378+Arisu-py@users.noreply.github.com>
2024-04-06 21:07:23 +0400

    Update p2.py

--- p2.py
+++ p2.py
@@ -1,22 +1,11 @@
-# peizashi marsa
-from flask import Flask, url_for, request, render_template
+# super parser
+import sys
+import argparse
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def f():
-    return "<title>hello</title><h1>Миссия Колонизация Марса<h1>"
-
-
-@app.route('/test_carousel')
-def return_carousel():
-    pics = [(f"{url_for('static', filename='img/1.jpeg')}", "first"),
-                        (f"{url_for('static', filename='img/2.jpeg')}", "second"),
-                        (f"{url_for('static', filename='img/3.jpeg')}", "third")
-                        ]
-    return render_template('test_carousel.html', title='Карусель', pics=pics)
-
-
-if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+parser = argparse.ArgumentParser()
+parser.add_argument('arg', nargs='*')
+my_args = parser.parse_args()
+if my_args.arg:
+    print(*my_args.arg, sep='\n')
+else:
+    print('no args')
