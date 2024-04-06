bdd4c3ed89ae989e6cd0cb6d71b56f9e837642cc
Arisu-py <150816378+Arisu-py@users.noreply.github.com>
2024-04-06 21:07:55 +0400

    Update p2.py

--- p2.py
+++ p2.py
@@ -1,11 +1,29 @@
-# super parser
+# predskasatel pola
 import sys
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('arg', nargs='*')
+parser = argparse.ArgumentParser(
+    description="test args")
+
+
+parser.add_argument('--barbie',
+                    type=int,
+                    default=50)
+parser.add_argument('--cars',
+                    type=int,
+                    default=50)
+parser.add_argument('--movie',
+                    default='other',
+                    choices=['melodrama', 'football', 'other']                    choices=['melodrama', 'football', 'other'])
+)
+
 my_args = parser.parse_args()
-if my_args.arg:
-    print(*my_args.arg, sep='\n')
-else:
-    print('no args')
+if my_args.barbie not in range(101):
+    my_args.barbie = 50
+if my_args.cars not in range(101):
+    my_args.cars = 50
+d = {'football': 100, 'melodrama': 0, 'other': 50}
+boy = int((100 - my_args.barbie + my_args.cars + d[my_args.movie]) / 3)
+girl = 100 - boy
+print(f"boy: {boy}")
+print(f"girl: {girl}")
