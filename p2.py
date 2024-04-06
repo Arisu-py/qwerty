0949135a485a56f31f60e4c266feb57767dfe624
Arisu-py <150816378+Arisu-py@users.noreply.github.com>
2024-04-06 19:31:21 +0400

    Update p2.py
    17

--- p2.py
+++ p2.py
@@ -1,22 +1,9 @@
-# calculator 2.0
+# slovar is parametrov
 import sys
 
-if len(sys.argv) > 1:
-    a = 1
-    c = 0
-    for i in range(1, len(sys.argv)):
-        if sys.argv[i].isdigit() and float(sys.argv[i]).is_integer():
-            c += int(sys.argv[i]) * a
-            a *= -1
-        elif sys.argv[i].isdigit():
-            c = 0
-            a = 2
-            break
-        else:
-            a = 3
-            print('ValueError')
-            break
-    if a == 1 or a == -1:
-        print(c)
-else:
-    print("NO PARAMS")
+test = sys.argv[1:]
+sp = {a.split('=')[0]: a.split('=')[1] for a in test if a != '--sort'}
+if '--sort' in test:
+    sp = dict(sorted(sp.items()))
+for a in sp:
+    print(f'Key: {a} Value: {sp[a]}')
