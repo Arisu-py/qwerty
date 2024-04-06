032a81bec7dca706aa2430c4db26087c7ed86dba
Arisu-py <150816378+Arisu-py@users.noreply.github.com>
2024-04-06 19:23:29 +0400

    Update p2.py
    12

--- p2.py
+++ p2.py
@@ -1,15 +1,25 @@
-from yandex_testing_lesson import is_palindrome
+from yandex_testing_lesson import is_prime
 
 
 def check():
-    if not is_palindrome(""):
+    if not is_prime(2):
         return 'NO'
-    if not is_palindrome("a"):
+    if not is_prime(3):
         return 'NO'
-    if not is_palindrome("aba"):
+    if not is_prime(5):
         return 'NO'
-    if is_palindrome("abc"):
+    if not is_prime(7):
         return 'NO'
+    if is_prime(4):
+        return 'NO'
+    if is_prime(22):
+        return 'NO'
+    # if is_prime(1):
+    #     return 'NO'
+    # if is_prime(0):
+    #     return 'NO'
+    # if is_prime(-1):
+    #     return 'NO'
     return 'YES'
 
 
