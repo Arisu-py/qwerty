8e9a150230b798fd0732769b4b6f63a4f5e8d2c6
Arisu-py <150816378+Arisu-py@users.noreply.github.com>
2024-04-06 21:03:43 +0400

    Update p2.py
    21

--- p2.py
+++ p2.py
@@ -1,4 +1,4 @@
-# data i vremia
+# timer bot20
 import logging
 import time as t
 from telegram.ext import Application, MessageHandler, filters, CommandHandler
@@ -36,6 +36,48 @@ async def date(update, context):
     await update.message.reply_text(t.asctime())
 
 
+def remove_job_if_exists(name, context):
+    """Удаляем задачу по имени.
+    Возвращаем True если задача была успешно удалена."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+# Обычный обработчик, как и те, которыми мы пользовались раньше.
+async def set_timer(update, context):
+    """Добавляем задачу в очередь"""
+    global timer
+    chat_id = update.effective_message.chat_id
+    timer = int(context.args[0])
+    if timer < 0:
+        await update.effective_message.reply_text('К сожалению, невозможно вернуться назад.')
+        return
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    context.job_queue.run_once(task, timer, chat_id=chat_id, name=str(chat_id), data=timer)
+
+    text = f'Вернусь через {timer} с.!'
+    if job_removed:
+        text += ' Старая задача удалена.'
+    await update.effective_message.reply_text(text)
+
+
+async def task(context):
+    """Выводит сообщение"""
+    await context.bot.send_message(context.job.chat_id, text=f'КУКУ! {timer}c. прошли!')
+
+
+async def unset(update, context):
+    """Удаляет задачу, если пользователь передумал"""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = 'Таймер отменен!' if job_removed else 'У вас нет активных таймеров'
+    await update.message.reply_text(text)
+
+
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
     text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
@@ -44,6 +86,8 @@ def main():
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("time", time))
     application.add_handler(CommandHandler("date", date))
+    application.add_handler(CommandHandler("set", set_timer))
+    application.add_handler(CommandHandler("unset", unset))
     application.run_polling()
 
 
