02c8de29ca6e392990594326cc18a11b11c724b5
Arisu-py <150816378+Arisu-py@users.noreply.github.com>
2024-04-06 19:30:16 +0400

    Update p2.py
    16

--- p2.py
+++ p2.py
@@ -1,8 +1,22 @@
-# calculator 1.0
+# calculator 2.0
 import sys
 
-test = sys.argv[1:]
-if len(test) != 2 or not (test[0].isdigit() and test[0].isdigit()):
-    print(0)
+if len(sys.argv) > 1:
+    a = 1
+    c = 0
+    for i in range(1, len(sys.argv)):
+        if sys.argv[i].isdigit() and float(sys.argv[i]).is_integer():
+            c += int(sys.argv[i]) * a
+            a *= -1
+        elif sys.argv[i].isdigit():
+            c = 0
+            a = 2
+            break
+        else:
+            a = 3
+            print('ValueError')
+            break
+    if a == 1 or a == -1:
+        print(c)
 else:
-    print(int(test[0]) + int(test[1]))
+    print("NO PARAMS")
