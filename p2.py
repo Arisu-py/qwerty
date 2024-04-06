1147eff64d6a29afb7de262ce40ea26eca48d0b1
Arisu-py <150816378+Arisu-py@users.noreply.github.com>
2024-04-06 19:24:50 +0400

    Update p2.py

--- p2.py
+++ p2.py
@@ -1,12 +1,22 @@
-def is_prime(n):
-    if n < 2:
-        return 'NO'
-    elif n / 2 == 2:
-        return 'NO'
-    for divisor in range(2, n // 2 + 1):
-        if n % divisor == 0:
-            return 'NO'
-    return 'YES'
+import os
+
+p = os.getcwd()
 
 
-print(is_prime(int(input())))
+def human_read_format(size):
+    n = ['Б', 'КБ', 'МБ', 'ГБ']
+    m = size
+    c = 0
+    while m / 1024 >= 1:
+        m = round(m / 1024)
+        c += 1
+    return f'{m}{n[c]}'
+
+
+def get_files_sizes():
+    sp = []
+    for a in os.listdir():
+        if os.path.isfile(a):
+            s = f'{p}/{a}'
+            sp.append(f'{a} {human_read_format(os.path.getsize(s))}')
+    return ('\n').join(sp)
