4add87aead0dacb727072e0d637d1a53e461f585
Arisu-py <150816378+Arisu-py@users.noreply.github.com>
2024-04-06 21:06:02 +0400

    Update p2.py

--- p2.py
+++ p2.py
@@ -1,8 +1,22 @@
-# 2+2
-import requests
+# peizashi marsa
+from flask import Flask, url_for, request, render_template
+
+app = Flask(__name__)
 
 
-url = f"{input()}:{input()}?a={input()}&b={input()}"
-response = requests.get(url).json()
-print(*sorted(response['result']))
-print(response['check'])
+@app.route('/')
+def f():
+    return "<title>hello</title><h1>Миссия Колонизация Марса<h1>"
+
+
+@app.route('/test_carousel')
+def return_carousel():
+    pics = [(f"{url_for('static', filename='img/1.jpeg')}", "first"),
+                        (f"{url_for('static', filename='img/2.jpeg')}", "second"),
+                        (f"{url_for('static', filename='img/3.jpeg')}", "third")
+                        ]
+    return render_template('test_carousel.html', title='Карусель', pics=pics)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
