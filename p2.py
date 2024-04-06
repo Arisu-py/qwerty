d224e55373738a417b4e5e16ac35df6a1472aafe
Arisu-py <150816378+Arisu-py@users.noreply.github.com>
2024-04-06 19:32:22 +0400

    Update p2.py
    18

--- p2.py
+++ p2.py
@@ -1,9 +1,30 @@
-# slovar is parametrov
+# super cat
 import sys
+import os
 
 test = sys.argv[1:]
-sp = {a.split('=')[0]: a.split('=')[1] for a in test if a != '--sort'}
-if '--sort' in test:
-    sp = dict(sorted(sp.items()))
-for a in sp:
-    print(f'Key: {a} Value: {sp[a]}')
+name = ''
+for el in test:
+    if '.txt' in el:
+        name = el
+if os.access(name, os.F_OK):
+    with open(name) as in_file:
+        lines = [line.rstrip('\n') for line in in_file.readlines()]
+    if '--sort' in test:
+        lines.sort()
+        test.pop(test.index('--sort'))
+    if len(test) > 1:
+        f = True
+        if '--num' in test:
+            for i in range(len(lines)):
+                print(i, lines[i])
+            f = False
+        if '--count' in test:
+            if f:
+                print(*lines, sep='\n')
+            print(f'rows count: {len(lines)}')
+    else:
+        print(*lines, sep='\n')
+else:
+    print('ERROR')
+
