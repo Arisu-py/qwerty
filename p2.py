591571e65fdba64333a0abb0dae2fb083d1b9148
Arisu-py <150816378+Arisu-py@users.noreply.github.com>
2024-04-06 21:09:42 +0400

    Update p2.py

--- p2.py
+++ p2.py
@@ -1,19 +1,20 @@
-# kopirovanie filov
+# kalculyator 30
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("source_file")
-parser.add_argument("source_receiver")
-parser.add_argument('--upper', dest='up', action='store_true')
-parser.add_argument('--lines', dest='line')
+parser = argparse.ArgumentParser(
+    description="test args")
+
+parser.add_argument('ar', nargs='*')
 args = parser.parse_args()
-with open(args.source_file) as in_file:
-    lines = [line for line in in_file.readlines()]
-if args.line:
-    if int(args.line) > len(lines):
-        args.line = len(lines)
-    lines = lines[:int(args.line)]
-if args.up:
-    lines = [a.upper() for a in lines]
-with open(args.source_receiver, 'w') as out_file:
-    print(*lines, sep='', end='', file=out_file)
+
+if not args.ar:
+    print("NO PARAMS")
+elif len(args.ar) == 1:
+    print("TOO FEW PARAMS")
+elif len(args.ar) > 2:
+    print("TOO MANY PARAMS")
+else:
+    if args.ar[0].isdigit() and args.ar[1].isdigit():
+        print(int(args.ar[0]) + int(args.ar[1]))
+    else:
+        print('ValueError')
