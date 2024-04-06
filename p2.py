34cb88a8ac64cefbdf95f1511678358b7459e874
Arisu-py <150816378+Arisu-py@users.noreply.github.com>
2024-04-06 21:09:14 +0400

    Update p2.py

--- p2.py
+++ p2.py
@@ -1,18 +1,19 @@
-# sosdanie i ispolzovanie modulei
+# kopirovanie filov
 import argparse
-import os
 
-
-def count_lines(p):
-    if os.access(p, os.F_OK):
-        with open(p) as in_file:
-            lines = [line.rstrip('\n') for line in in_file.readlines()]
-        return len(lines)
-    return 0
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--file', dest='file')
-    my_args = parser.parse_args()
-    print(count_lines(my_args.file))
+parser = argparse.ArgumentParser()
+parser.add_argument("source_file")
+parser.add_argument("source_receiver")
+parser.add_argument('--upper', dest='up', action='store_true')
+parser.add_argument('--lines', dest='line')
+args = parser.parse_args()
+with open(args.source_file) as in_file:
+    lines = [line for line in in_file.readlines()]
+if args.line:
+    if int(args.line) > len(lines):
+        args.line = len(lines)
+    lines = lines[:int(args.line)]
+if args.up:
+    lines = [a.upper() for a in lines]
+with open(args.source_receiver, 'w') as out_file:
+    print(*lines, sep='', end='', file=out_file)
